59f663507342c23b841acab77cb6cf5ff2adbdaa
Alexandre Proença <alexandre.proenca@hotmail.com.br>
2015-10-25 15:54:14 -0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Seguindo o mesmo ritual de sempre
+    cd master
+    sudo npm install
+    bower install
+    sudo gem install sass
+    sudo gem install compass
+    gulp --usesass
---
+++ master/js/modules/controllers/access-login.js
@@ -0,0 +1,41 @@
+/**=========================================================
+ * Module: access-login.js
+ * Demo for login api
+ =========================================================*/
+
+App.controller('LoginFormController', ['$scope', '$http', '$state', function($scope, $http, $state) {
+
+  // bind here all data from the form
+  $scope.account = {};
+  // place the message if something goes wrong
+  $scope.authMsg = '';
+
+  $scope.login = function() {
+    $scope.authMsg = '';
+
+    if($scope.loginForm.$valid) {
+
+      $http
+        .post('https://backend-morandofloripa.herokuapp.com/v1/rest-auth/login/', {username: $scope.account.username, password: $scope.account.password})
+        .then(function(response) {
+          // assumes if ok, response is an object with some data, if not, a string with error
+          // customize according to your api
+          if ( !response.status == 200 ) {
+            console.log("sf",response);
+            $scope.authMsg = 'Incorrect credentials.';
+          }else{
+
+            $state.go('app.dashboard');
+          }
+        }, function(x) {
+          $scope.authMsg = 'Server Request Error';
+        });
+    }
+    else {
+      // set as dirty if the user click directly to login so we show the validation messages
+      $scope.loginForm.username.$dirty = true;
+      $scope.loginForm.account_password.$dirty = true;
+    }
+  };
+
+}]);
